2fd4c4120343a73afeda27151689cb895808fb97
The-Memin <guillejuma@outlook.es>
2025-05-11 15:39:48 -0600

    Part 1 - Exercise 1.11: Unicafe, step 6

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -3,14 +3,14 @@ const Statistics = (props)=>{
     return(
         <div className="statistics">
             <h2>Statistics</h2>
-            <ul>
+            <table>
                 <StatisticLine text='good' value={props.good}/>
                 <StatisticLine text='neutral' value={props.neutral}/>
                 <StatisticLine text='bad' value={props.bad}/>
                 <StatisticLine text='all' value={props.all}/>
                 <StatisticLine text='average' value={props.average}/>
                 <StatisticLine text='positive' value={`${props.positive} %`}/>
-            </ul>
+            </table>
         </div>
     )
 }
