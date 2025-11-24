d7e0ce2fbedeeb3e02eac952e7044940e47cc42c
The-Memin <guillejuma@gmail.com>
2025-11-24 13:15:53 -0600

    Part 8 - Exercise 8.7 Updating the birth year of an author

--- part8/library/library-backend.js
+++ part8/library/library-backend.js
@@ -128,6 +128,11 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -165,6 +170,15 @@ const resolvers = {
         authors = authors.concat(newAuthor)
       }
       return newBook
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+      if(!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
