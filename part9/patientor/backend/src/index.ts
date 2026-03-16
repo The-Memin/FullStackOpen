b1d4c82c1d5bad7afb90abd63d8f86c61bc0e270
The-Memin <guillejuma@gmail.com>
2026-03-16 13:19:33 -0600

    Part 9 - Exercise 9.11: Patientor backend, step 4

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import diagnoseRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
+
 const app = express();
 app.use(express.json());
 const PORT = 3001;
 
 app.use(cors());
 app.use('/api/diagnoses', diagnoseRouter);
+app.use('/api/patients/', patientRouter);
 
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
---
+++ part9/patientor/backend/src/services/patientService.ts
@@ -0,0 +1,23 @@
+import patientsData from '../../data/patients';
+import { Patient, NonSensitivePatientData } from '../types';
+
+const patients: Patient[] = patientsData;
+
+const getPatients = (): Patient[] => {
+    return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatientData[] => {
+    return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+        id,
+        name,
+        dateOfBirth,
+        gender,
+        occupation
+    }));
+};
+
+export default {
+    getPatients,
+    getNonSensitivePatients
+};
