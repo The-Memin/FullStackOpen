bcce56827d10582891f05285e8c1a99f4690f138
The-Memin <guillejuma@gmail.com>
2026-03-16 12:50:15 -0600

    Part 9 - Exercise 9.10: Patientor backend, step 3

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
 const app = express();
 app.use(express.json());
 const PORT = 3001;
 
 app.use(cors());
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server runing on PORT ${PORT}`);
