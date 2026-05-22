69f31d633b396a51f860073beaf09bea6809241e
The-Memin <guillejuma@outlook.es>
2026-05-22 17:34:56 -0600

    Part 9 - Exercise 9.23: Patientor, step 4

--- part9/patientor/frontend/src/components/PatientDetails.tsx
+++ part9/patientor/frontend/src/components/PatientDetails.tsx
@@ -24,9 +24,32 @@ const PatientDetails = () => {
         <div className="mt-10">
             <div>
                 <h2 className="text-2xl font-semibold my-4">{patient?.name} {genderIcon}</h2>
-                <p><span className="font-semibold">ssn:</span> {patient?.ssn}</p>
-                <p><span className="font-semibold">date of birth:</span> {patient?.dateOfBirth}</p>
-                <p><span className="font-semibold">occupation:</span> {patient?.occupation}</p>
+                <div>
+                    <p><span className="font-semibold">ssn:</span> {patient?.ssn}</p>
+                    <p><span className="font-semibold">occupation:</span> {patient?.occupation}</p>
+                </div>
+                <div className="mt-8">
+                    <h3 className="font-bold text-xl">entries</h3>
+                    <div>
+                        {
+                            patient?.entries.map( entry => (
+                                <div key={entry.id} className="border border-gray-300 rounded p-4 my-4">
+                                    <div className="flex gap-3">
+                                        <span>{entry.date}</span>
+                                        <p className="italic">{entry.description}</p>
+                                    </div>
+                                    <ul className="list-disc ml-6 mt-2">
+                                        {
+                                            entry.diagnosisCodes?.map( code => (
+                                                <li key={code} className="text-sm text-gray-900">{code}</li>
+                                            ))
+                                        }
+                                    </ul>
+                                </div>
+                            )) 
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     );
